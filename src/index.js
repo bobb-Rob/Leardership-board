3a9811d5fa313eff82d142e238d27dfeb09290e2
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-14 13:47:03 +0100

    fix lighthouse issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import renderHeader from './modules/header.js';
 import renderRecentScore from './modules/recentScore.js';
 import renderAddScore from './modules/addScore.js';
 import './style.css';
 
-renderHeader();
 renderRecentScore();
 renderAddScore();
